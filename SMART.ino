cfe223e76febc4b85053f3476e3dd7f46ba67e5d
jetze003 <jetze003@umn.edu>
2018-05-22 16:51:03 -0500

    LED for video

--- SMART.ino
+++ SMART.ino
@@ -2,7 +2,8 @@
 //#include <CopernicusGPS.h>
 
 #define smartPin 10
-#define cutTime .3 //Time in increments of 10 seconds
+#define cutTime 70 //Time in minutes
+#define light 9
 
 bool released = false;
 
@@ -14,16 +15,21 @@ Smart smart = Smart(smartPin);
 void setup() {
   smart.initialize();
   //gps.initialize();
+  pinMode(light, OUTPUT);
   Serial.begin(115200);
   Serial.println("Testing");
 }
 
 void loop() {
   //gps.update();
+  if (!released && (millis()/60000.0 > cutTime-.2)){
+    digitalWrite(light, HIGH);}
   if (!released && (millis()/60000.0 > cutTime)) {
     Serial.print("Releasing");
     smart.release();
+    
     released = true;
   }
-  delay(1000);
+  delay(5000);
+  digitalWrite(light, LOW);
 }
